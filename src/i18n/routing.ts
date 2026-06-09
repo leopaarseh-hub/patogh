4320843c9cefdb8155103a99617526d3ba9acd24
leopaarseh-hub <leopaarseh@gmail.com>
2026-06-09 12:27:46 +0200

    Delete src directory

--- src/i18n/routing.ts
+++
@@ -1,6 +0,0 @@
-import { defineRouting } from 'next-intl/routing';
-export const routing = defineRouting({
-  locales: ['de', 'fa'],
-  defaultLocale: 'de',
-  localePrefix: 'as-needed',
-});
--- src/lib/utils.ts
+++
@@ -1,9 +0,0 @@
-import { clsx, type ClassValue } from 'clsx';
-import { twMerge } from 'tailwind-merge';
-export const cn = (...i: ClassValue[]) => twMerge(clsx(i));
-export const fmt = (n: number) => Number.isInteger(n) ? `${n}€` : `${n.toFixed(1).replace('.',',')}€`;
-export const ff  = (rtl: boolean) => rtl ? "'Vazirmatn','Tahoma',sans-serif" : "'Inter','Helvetica',sans-serif";
-export const PHONE = '0211 26107041';
-export const IG    = 'patogh2026';
-export const WA    = 'https://wa.me/492112610741';
-export const ADDR  = 'Reisholzer Str. 25, 40231 Düsseldorf';
